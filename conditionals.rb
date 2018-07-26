b8e0bb4d828b5cf72b3ecf532dc2adb28ec6ffa3
Jason Solomon <jasonmaxsolomon@gmail.com>
2018-07-26 12:46:41 +0000

    automatically backed up by learn

---
+++ conditionals.rb
@@ -0,0 +1,20 @@
+# def example(x)
+# if x > 5
+#   print "I am big"
+#   elsif x==5
+#   print "I am medium"
+# else
+#   print "I am small"
+# end
+# end 
+
+# example(6)
+
+x=4
+if x > 5
+  print "I am big"
+  elsif x==4
+  print "I am medium"
+else
+  print "I am small"
+end
---
+++ loops.rb
@@ -0,0 +1,12 @@
+# num_of_hotdogs_eaten = 0 
+# while num_of_hotdogs_eaten < 7 
+#   num_of_hotdogs_eaten += 1 
+#   puts "you have now eaten #{num_of_hotdogs_eaten}"
+# end
+
+
+dogs_names = ["blue", "red", "yellow", "green"]
+dogs_names << ("Rihanna")
+puts dogs_names
+
+
